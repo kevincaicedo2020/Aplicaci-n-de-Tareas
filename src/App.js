8c3be103d452d84272b6c41efbdda386c880fac5
kevin caicedo <kevincaicedo-liga@hotmail.com>
2022-06-01 16:20:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ListaDeTareas from './componentes/ListaDeTareas';
+/* Descripcion uso de react icons-fragmentos-los hooks al parecer son los useState-uuid es una libreria para generar un identificador unico */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='tareas-lista-principal'>
+        <h1>Mi lista de tareas</h1>
+        <ListaDeTareas />
+      </div>
     </div>
   );
 }
